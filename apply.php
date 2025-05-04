1427cf0ba5b1d2699d27d626c51c62526b1a49ff
AshKmo <ashkmo33@gmail.com>
2025-05-04 16:17:43 +1000

    convert each page to php and separate the headers, footers and common meta tags into .inc files

---
+++ apply.php
@@ -0,0 +1,304 @@
+<!DOCTYPE html>
+
+<!-- this page can be accessed on Github Pages at https://ashkmo.github.io/cos10026-part1/apply.php -->
+
+<!-- the page language is set to English -->
+<html lang="en">
+
+<head>
+	<!-- include some common meta tags shared between all regular pages -->
+	<?php include "meta.inc"; ?>
+
+	<!-- set the page description -->
+	<meta name="description" content="Apply for a position at Tolstra">
+
+	<!-- set keywords for SEO -->
+	<meta name="keywords" content="Tolstra, jobs, application">
+
+	<!-- set the page title -->
+	<title>Applications</title>
+</head>
+
+<body>
+	<!-- include the page header -->
+	<?php include "header.inc" ?>
+
+	<!-- define the main body content of the page -->
+	<main>
+		<h1>Apply for a position</h1>
+
+		<p>Tolstra is always committed to providing premium employment opportunities to those who need them the most.
+			Please submit your application below, and welcome aboard!</p>
+
+		<!-- HTML form containing all fields for submission -->
+		<form method="POST" action="https://mercury.swin.edu.au/it000000/formtest.php">
+			<fieldset>
+				<legend>Personal details</legend>
+
+				<div id="apply-personal-text-fields-container">
+					<p>
+						<label for="apply-first-name">First name: </label>
+						<!-- pattern created using RegExr's regular expression parser and cheat sheet -->
+						<!-- availabe at https://regexr.com/ -->
+						<input type="text" name="first-name" id="apply-first-name" pattern="[a-zA-Z]{1,20}" size="20"
+							maxlength="20" placeholder="Your first name here" required>
+					</p>
+
+					<p>
+						<label for="apply-last-name">Last name: </label>
+						<!-- pattern created using RegExr's regular expression parser and cheat sheet -->
+						<!-- availabe at https://regexr.com/ -->
+						<input type="text" name="last-name" id="apply-last-name" pattern="[a-zA-Z]{1,20}" size="20"
+							maxlength="20" placeholder="Your last name here" required>
+					</p>
+
+					<p>
+						<label for="apply-email">Email address: </label>
+						<!-- pattern created using RegExr's regular expression parser and cheat sheet -->
+						<!-- availabe at https://regexr.com/ -->
+						<input type="text" name="email" id="apply-email" placeholder="asdf@asdf.com"
+							pattern="\w+@([\w\-]+\.)+[\w\-]{2,}" required>
+					</p>
+
+					<p>
+						<label for="apply-phone">Phone number: </label>
+						<!-- pattern created using RegExr's regular expression parser and cheat sheet -->
+						<!-- availabe at https://regexr.com/ -->
+						<input type="text" name="phone" id="apply-phone" placeholder="0000 000 000" maxlength="12"
+							pattern="[0-9 ]{8,12}" required>
+					</p>
+
+					<p>
+						<label for="apply-address">Street address: </label>
+						<input type="text" id="apply-address" name="address" size="40" maxlength="40"
+							placeholder="Your street address here" required>
+					</p>
+
+					<p>
+						<label for="apply-town">Suburb/town: </label>
+						<input type="text" id="apply-town" name="town" size="40" maxlength="40"
+							placeholder="Your suburb or town name here" required>
+					</p>
+				</div>
+
+				<p>
+					<label for="apply-state">State: </label>
+					<select name="state" id="apply-state" required>
+						<option value="">Please select</option>
+						<option value="VIC">VIC</option>
+						<option value="NSW">NSW</option>
+						<option value="QLD">QLD</option>
+						<option value="NT">NT</option>
+						<option value="WA">WA</option>
+						<option value="SA">SA</option>
+						<option value="TAS">TAS</option>
+						<option value="ACT">ACT</option>
+					</select>
+				</p>
+
+				<p>
+					<label for="apply-postcode">Postcode: </label>
+					<!-- pattern created using RegExr's regular expression parser and cheat sheet -->
+					<!-- availabe at https://regexr.com/ -->
+					<input type="text" name="postcode" id="apply-postcode" maxlength="4" minlength="4" size="4"
+						placeholder="0000" pattern="[0-9]{4}" required>
+				</p>
+
+				<p>
+					<label for="apply-date-of-birth">Date of birth: </label>
+					<input type="text" name="date-of-birth" id="apply-date-of-birth" placeholder="dd/mm/yyyy" size="10"
+						maxlength="10" pattern="[0-9]{2}\/[0-9]{2}\/[0-9]{4}" required>
+				</p>
+
+				<!-- radio fieldset for gender selection -->
+				<fieldset id="apply-fieldset-gender">
+					<legend>Gender</legend>
+
+					<p>
+						<input type="radio" name="gender" value="male" id="apply-gender_male" required>
+						<label for="apply-gender_male">Male</label>
+					</p>
+
+					<p>
+						<input type="radio" name="gender" value="female" id="apply-gender_female" required>
+						<label for="apply-gender_female">Female</label>
+					</p>
+
+					<p>
+						<input type="radio" name="gender" value="other" id="apply-gender_other" required>
+						<label for="apply-gender_other">Other</label>
+					</p>
+
+					<p id="apply-specific-gender-paragraph">
+						<label for="apply-specific-gender">Description: </label>
+						<input type="text" name="specific-gender" id="apply-specific-gender"
+							placeholder="Please describe your gender here" size="30">
+					</p>
+
+					<p>
+						<input type="radio" name="gender" value="unspecified" id="apply-gender_unspecified" required
+							checked>
+						<label for="apply-gender_unspecified">Prefer not to say</label>
+					</p>
+				</fieldset>
+			</fieldset>
+
+			<fieldset id="apply-employment-details">
+				<legend>Employment details</legend>
+
+				<!-- set all the options for the job selection box and create the selection box -->
+				<p>
+					<label for="apply-job-reference-number">Position of interest: </label>
+					<select name="job-reference-number" id="apply-job-reference-number" required>
+						<option value="">Please select</option>
+						<option value="IT427">IT Support Technician (IT427)</option>
+						<option value="DA193">Data Analyst (DA193)</option>
+						<option value="BC279">Blockchain Developer (BC279)</option>
+						<option value="QA666">QA Tester (QA666)</option>
+					</select>
+				</p>
+
+				<!-- a series of checkboxes for each required technical skill for each job -->
+				<!-- the relevant checkboxes for each job are shown only when that job is selected above -->
+				<!-- this is done by applying a class to each checkbox indicating the job to which it is assigned -->
+				<!-- although this means that there might be values sent to the server that are not necessarily relevant for the selected job, the server should (in practice) be able to ignore them based on which job was selected -->
+				<fieldset>
+					<legend>Required technical skills:</legend>
+
+					<div class="apply-checkbox-set-container">
+						<div class="apply-checkbox-container apply-checkbox-set-support apply-checkbox-set-analyst">
+							<input type="checkbox" name="required-technical-skills[]"
+								id="apply-required-technical-skills_sql" value="sql" checked>
+							<label for="apply-required-technical-skills_sql">Understanding of SQL</label>
+						</div>
+
+						<div class="apply-checkbox-container apply-checkbox-set-support">
+							<input type="checkbox" name="required-technical-skills[]"
+								id="apply-required-technical-skills_python" value="python" checked>
+							<label for="apply-required-technical-skills_python">Understanding of Python</label>
+						</div>
+
+						<div class="apply-checkbox-container apply-checkbox-set-support">
+							<input type="checkbox" name="required-technical-skills[]"
+								id="apply-required-technical-skills_networks" value="networks" checked>
+							<label for="apply-required-technical-skills_networks">Understanding of networks</label>
+						</div>
+
+						<div class="apply-checkbox-container apply-checkbox-set-analyst">
+							<input type="checkbox" name="required-technical-skills[]"
+								id="apply-required-technical-skills_excel" value="excel" checked>
+							<label for="apply-required-technical-skills_excel">Proficiency in Microsoft Excel</label>
+						</div>
+
+						<div class="apply-checkbox-container apply-checkbox-set-analyst">
+							<input type="checkbox" name="required-technical-skills[]"
+								id="apply-required-technical-skills_vistools" value="vistools" checked>
+							<label for="apply-required-technical-skills_vistools">Experience with data visualisation
+								tools</label>
+						</div>
+
+						<div class="apply-checkbox-container apply-checkbox-set-analyst">
+							<input type="checkbox" name="required-technical-skills[]"
+								id="apply-required-technical-skills_problem_solving" value="problem_solving" checked>
+							<label for="apply-required-technical-skills_problem_solving">Strong analytical and
+								problem-solving skills</label>
+						</div>
+
+						<div class="apply-checkbox-container apply-checkbox-set-analyst">
+							<input type="checkbox" name="required-technical-skills[]"
+								id="apply-required-technical-skills_bachelor_data" value="bachelor_data" checked>
+							<label for="apply-required-technical-skills_bachelor_data">Bachelor's degree in Data
+								Science</label>
+						</div>
+
+						<div class="apply-checkbox-container apply-checkbox-set-analyst">
+							<input type="checkbox" name="required-technical-skills[]"
+								id="apply-required-technical-skills_bachelor_stats" value="bachelor_stats" checked>
+							<label for="apply-required-technical-skills_bachelor_stats">Bachelor's degree in
+								Statistics</label>
+						</div>
+
+						<div class="apply-checkbox-container apply-checkbox-set-qa">
+							<input type="checkbox" name="required-technical-skills[]"
+								id="apply-required-technical-skills_qa_testing" value="qa_testing" checked>
+							<label for="apply-required-technical-skills_qa_testing">Proven experience in software
+								quality assurance or testing</label>
+						</div>
+
+						<div class="apply-checkbox-container apply-checkbox-set-qa">
+							<input type="checkbox" name="required-technical-skills[]"
+								id="apply-required-technical-skills_qa_methodologies" value="qa_methodologies" checked>
+							<label for="apply-required-technical-skills_qa_methodologies">Strong understanding of QA
+								methodologies, tools, and processes</label>
+						</div>
+
+						<div class="apply-checkbox-container apply-checkbox-set-qa">
+							<input type="checkbox" name="required-technical-skills[]"
+								id="apply-required-technical-skills_test_cases" value="test_cases" checked>
+							<label for="apply-required-technical-skills_test_cases">Ability to write comprehensive test
+								plans and test cases</label>
+						</div>
+
+						<div class="apply-checkbox-container apply-checkbox-set-qa">
+							<input type="checkbox" name="required-technical-skills[]"
+								id="apply-required-technical-skills_bug_tracking" value="bug_tracking" checked>
+							<label for="apply-required-technical-skills_bug_tracking">Familiarity with bug tracking
+								tools</label>
+						</div>
+
+						<div class="apply-checkbox-container apply-checkbox-set-qa">
+							<input type="checkbox" name="required-technical-skills[]"
+								id="apply-required-technical-skills_sdlc" value="sdlc" checked>
+							<label for="apply-required-technical-skills_sdlc">Understanding of the Software Development
+								Life Cycle</label>
+						</div>
+
+						<div class="apply-checkbox-container apply-checkbox-set-blockchain">
+							<input type="checkbox" name="required-technical-skills[]"
+								id="apply-required-technical-skills_smart_contract" value="smart_contract" checked>
+							<label for="apply-required-technical-skills_smart_contract">Proficiency in a smart contract
+								language</label>
+						</div>
+
+						<div class="apply-checkbox-container apply-checkbox-set-blockchain">
+							<input type="checkbox" name="required-technical-skills[]"
+								id="apply-required-technical-skills_architecture" value="architecture" checked>
+							<label for="apply-required-technical-skills_architecture">Strong understanding of blockchain
+								architecture and principles</label>
+						</div>
+
+						<div class="apply-checkbox-container apply-checkbox-set-blockchain">
+							<input type="checkbox" name="required-technical-skills[]"
+								id="apply-required-technical-skills_blockchain_dev_tools" value="blockchain_dev_tools"
+								checked>
+							<label for="apply-required-technical-skills_blockchain_dev_tools">Familiarity with
+								blockchain development tools</label>
+						</div>
+
+						<div class="apply-checkbox-container apply-checkbox-set-blockchain">
+							<input type="checkbox" name="required-technical-skills[]"
+								id="apply-required-technical-skills_backend" value="backend" checked>
+							<label for="apply-required-technical-skills_backend">Proficiency in a backend programming
+								language</label>
+						</div>
+					</div>
+				</fieldset>
+
+				<div>
+					<label for="apply-other-skills">Other skills:</label>
+					<textarea id="apply-other-skills" name="other-skills"
+						placeholder="Enter any other skills you'd like to share here..."></textarea>
+				</div>
+			</fieldset>
+
+			<!-- add the buttons to submit and reset the form -->
+			<input type="submit" value="Apply" class="apply-fancy-button">
+			<input type="reset" value="Reset" class="apply-fancy-button apply-fancy-button-bad">
+		</form>
+	</main>
+
+	<!-- include the page footer -->
+	<?php include "footer.inc"; ?>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+
+<!-- this page can be accessed on Github Pages at https://ashkmo.github.io/cos10026-part1/ -->
+
+<!-- the page language is set to English -->
+<html lang="en">
+
+<head>
+	<!-- include some common meta tags shared between all regular pages -->
+	<?php include "meta.inc"; ?>
+
+	<!-- set the page description -->
+	<meta name="description" content="Tolstra home page">
+
+	<!-- set keywords for SEO -->
+	<meta name="keywords" content="Tolstra, telecomunications, phone, Internet">
+
+	<!-- set the page title -->
+	<title>Tolstra Home</title>
+</head>
+
+<body>
+	<!-- include the page header -->
+	<?php include "header.inc" ?>
+
+	<!-- define the main body content of the page -->
+	<main>
+
+		<h1>Tolstra</h1>
+
+		<div id="index-company-info">
+			<p>
+				Welcome to the home of <strong>Tolstra</strong>. We are a new and upcoming telecomunications company
+				striving to give individuals and commercial businesses the very best experience in mobile and other
+				commication services.
+			</p>
+		</div>
+
+		<br>
+
+		<!-- Company benefits banner -->
+		<div id="index-benefits">
+			<figure>
+				<!-- The code or content below was created using GenAI. GenAI prompt: “generate an image of a headset with a clay style as an icon format on a white background” -->
+				<img src="images/ringing_phone.png" alt="An image of a rigning clay phone">
+				<figcaption><strong>Unlimited Calls!</strong></figcaption>
+			</figure>
+			<figure>
+				<!-- The code or content below was created using GenAI. GenAI prompt: “generate an image of some coins with a clay style as an icon format on a white background” -->
+				<img src="images/coins.png" alt="An image of an two clay coins">
+				<figcaption><strong>Cheapest Plans Around!</strong></figcaption>
+			</figure>
+			<figure>
+				<!-- The code or content below was created using GenAI. GenAI prompt: “generate an image of a headset with a clay style as an icon format on a white background” -->
+				<img src="images/headset.png" alt="An image of a clay headset">
+				<figcaption><strong>24/7 Support!</strong></figcaption>
+			</figure>
+		</div>
+
+		<br>
+
+		<!-- The vision of our company -->
+		<div id="index-vision">
+			<h2>Our Vision</h2>
+			<p>
+				Here at <strong>Tolstra</strong>, we have long had a vision of a seamless and interconnected world. We
+				aim to try remove the perils long associated with setting up and managing telecomunications plans,
+				having all the best value setups one would need, all in one place.
+			</p>
+			<br>
+			<p>
+				If you happen to share our vision and think you would make a valued member of our team, we are currently
+				looking for new employees, and would love for you to join us. If this is you, please have a look at our
+				<a title="Job Positions" href="jobs.php">current availabilities</a>!
+			</p>
+		</div>
+
+		<!-- The pricing of the plans we offer -->
+		<div id="index-pricing">
+			<h2>Plan Pricing</h2>
+			<table>
+				<tr>
+					<th>Price</th>
+					<th>Features</th>
+					<th>Data</th>
+				</tr>
+				<tr>
+					<td>$12 / month</td>
+					<td>
+						<ul>
+							<li>Unlimited calls and texts</li>
+							<li>4G supported</li>
+						</ul>
+					</td>
+					<td>18G</td>
+				</tr>
+				<tr>
+					<td>$26 / month</td>
+					<td>
+						<ul>
+							<li>Unlimited calls and texts</li>
+							<li>5G supported</li>
+						</ul>
+					</td>
+					<td>50G</td>
+				</tr>
+				<tr>
+					<td>$50 / month</td>
+					<td>
+						<ul>
+							<li>Unlimited calls and texts</li>
+							<li>5G supported</li>
+							<li>Overseas calls and texts included</li>
+						</ul>
+					</td>
+					<td>100G</td>
+				</tr>
+			</table>
+		</div>
+
+	</main>
+
+	<!-- include the page footer -->
+	<?php include "footer.inc"; ?>
+</body>
+
+</html>
