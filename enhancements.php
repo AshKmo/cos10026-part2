b57d9667553f93c4ab6700cfaa5853701b9f07e4
AshKmo <ashkmo33@gmail.com>
2025-05-04 16:41:22 +1000

    added the page enhancements page and fixed the header and footer stylings

---
+++ enhancements.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+
+<!-- this page can be accessed on Github Pages at https://ashkmo.github.io/cos10026-part1/ -->
+
+<!-- the page language is set to English -->
+<html lang="en">
+
+<head>
+	<!-- include some common meta tags shared between all regular pages -->
+	<?php include "meta.inc"; ?>
+
+	<!-- set the page description -->
+	<meta name="description" content="Tolstra Webpage Enhancements">
+
+	<!-- set keywords for SEO -->
+	<meta name="keywords" content="Tolstra, website, enhancements">
+
+	<!-- set the page title -->
+	<title>Tolstra Home</title>
+</head>
+
+<body>
+	<!-- include the page header -->
+	<?php include "header.inc" ?>
+
+	<!-- define the main body content of the page -->
+	<main>
+
+		<h1>Webpage Enhancements</h1>
+
+		<p>Tolstra is proud to announce that a number of additional enhancements have been added to our website, including...</p>
+		<ul>
+			<!-- TODO: add some enhancements to the list -->
+			<li>TODO</li>
+		</ul>
+
+	</main>
+
+	<!-- include the page footer -->
+	<?php include "footer.inc"; ?>
+</body>
+
+</html>
