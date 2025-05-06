c4bf88cb2cb88e0cc570deb4c8b914bc89fcc9a9
106013868 <106013868@student.swin.edu.au>
2025-05-07 09:54:06 +1000

    created manage.php and added website template

---
+++ manage.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<!-- this page can be accessed on Github Pages at https://ashkmo.github.io/cos10026-part2/manage.php -->
+
+<!-- the page language is set to English -->
+<html lang="en">
+
+<head>
+	<!-- include some common meta tags shared between all regular pages -->
+	<?php include "meta.inc"; ?>
+
+	<!-- set the page description -->
+	<meta name="description" content="Tolstra - Manage EOIs">
+
+	<!-- set keywords for SEO -->
+	<meta name="keywords" content="Tolstra, telecommunications, Internet, phone, about">
+
+	<!-- set the page title -->
+	<title>Tolstra - Manage EOIs</title>
+</head>
+
+<body>
+	<!-- include the page header -->
+	<?php include "header.inc" ?>
+
+	<!-- define the main body content of the page -->
+	<main>
+
+
+	</main>
+
+	<!-- include the page footer -->
+	<?php include "footer.inc"; ?>
+</body>
+
+</html>
